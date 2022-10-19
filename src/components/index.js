e9629ac42001c3523142ef1bd13d3c4ddedea676
LLNet <me@luislucas.dk>
2022-10-19 21:04:25 +0200

    fix

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as CoolLightBox } from './components/CoolLightBox.vue'
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import CoolLightBox from './components/CoolLightBox.vue'
-
-export default {
-    install (app) {
-        app.component('CoolLightBox', CoolLightBox)
-    }
-}
